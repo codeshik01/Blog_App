c769cb915a3bff0ebc6d95e236f0f8b9494a8202
codeshik01 <164200054+codeshik01@users.noreply.github.com>
2025-08-21 06:58:37 +0530

    Update SubsTableItem.jsx

--- Components/AdminComponents/SubsTableItem.jsx
+++ Components/AdminComponents/SubsTableItem.jsx
@@ -4,7 +4,7 @@ const SubsTableItem = ({email,mongoId,deleteEmail,date}) => {
     
     const emailDate = new Date(date);
 
-  return (
+  return (  //jsx
     <tr className='bg-white border-b text-left'>
       <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
         {email?email:"No Email"}
